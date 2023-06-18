ee462e8b0494a3446b881622ac194ae340d02e20
Osman Onur BAŞ <onurbass.ob@gmail.com>
2023-06-18 21:58:53 +0300

    18062023

---
+++ PersonalTakipSistemi/src/main/java/BuroPersoneli.java
@@ -0,0 +1,10 @@
+public class BuroPersoneli extends Personal{
+private int fskj;
+private String sjfkjfs;
+
+    public BuroPersoneli(String isim, int maas, int fskj, String sjfkjfs) {
+        super(isim, maas);
+        this.fskj = fskj;
+        this.sjfkjfs = sjfkjfs;
+    }
+}
