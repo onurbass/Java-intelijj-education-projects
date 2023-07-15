51bfd9df8bb19843089e8116a6b3797ffc90cd8d
Osman Onur BAŞ <onurbass.ob@gmail.com>
2023-07-15 11:36:17 +0300

    15072023

---
+++ _60_Hibernate/src/main/java/com/basonur/model/AppMain.java
@@ -0,0 +1,33 @@
+package com.basonur.model;
+
+
+import com.basonur.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Date;
+
+public class AppMain {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        customer.setFirstname("Burak");
+        customer.setLastname("Delice");
+        customer.setAddress("Ankara");
+        customer.setCreationDate(new Date());
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            session.save(customer);
+
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            System.out.println("Hata : " + e);
+        } finally {
+            session.close();
+        }
+    }
+}
